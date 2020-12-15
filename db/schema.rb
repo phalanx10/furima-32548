f6523379bce5342b4bea4e2ff5abc1b8cfff0012
phalanx10 <phalanx.1379@gmail.com>
2020-12-15 10:37:20 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_015721) do
+ActiveRecord::Schema.define(version: 2020_12_15_012152) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "status", null: false
+    t.integer "delivery_days", null: false
+    t.integer "Shipping_area", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "categories", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_12_10_015721) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
