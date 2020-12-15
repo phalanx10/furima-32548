109380a104b3938ba7b77c32138d3ed1e9db7f2f
phalanx10 <phalanx.1379@gmail.com>
2020-12-15 14:26:40 +0900

    商品出品ページの表示(new)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signin, except: [:index]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def move_to_signin
+    unless user_signed_in?
+      authenticate_user!
+    end
   end
 end
