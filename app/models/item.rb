e4026b2bde722db6e977b28a3fd5aee0c3af46d7
phalanx10 <phalanx.1379@gmail.com>
2020-12-15 11:37:20 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,19 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   belongs_to :delivery_fee
   belongs_to :shipping_area
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :status_id        
+    validates :delivery_days_id 
+    validates :Shipping_area_id 
+    validates :delivery_fee_id 
+    validates :categories_id    
+  end
 end
