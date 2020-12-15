ca017a787c4875dbd162d327f4a398761fd0aec3
phalanx10 <phalanx.1379@gmail.com>
2020-12-15 12:01:42 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,15 +12,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
     validates :image
   end
 
   with_options numericality: { other_than: 1 } do
-    validates :status_id        
-    validates :delivery_days_id 
-    validates :Shipping_area_id 
-    validates :delivery_fee_id 
-    validates :categories_id    
+    validates :status_id
+    validates :delivery_days_id
+    validates :Shipping_area_id
+    validates :delivery_fee_id
+    validates :categories_id
   end
 end
