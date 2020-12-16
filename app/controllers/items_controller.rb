92695f2becaa53d988cb610ca955f88b5a409a05
phalanx10 <phalanx.1379@gmail.com>
2020-12-16 16:25:45 +0900

    ログアウト状態の詳細ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_signin, except: [:index]
+  before_action :move_to_signin, except: [:index, :show]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
