c6298a26a78e94359c7b15f50c9da1751f1a3a68
phalanx10 <phalanx.1379@gmail.com>
2020-12-16 12:48:47 +0900

    メンター指摘箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_signin, except: [:index]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
