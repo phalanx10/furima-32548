ed9cd177f0503af493564f7883771fe9e5e6fbe1
phalanx10 <phalanx.1379@gmail.com>
2020-12-17 12:37:02 +0900

    配送先・購入情報テーブルの作成

---
+++ app/models/destination_address.rb
@@ -0,0 +1,11 @@
+class DestinationAddress < ApplicationRecord
+  belongs_to :purchase_record
+
+  with_options presence: true do
+    validates :post_code
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+end
