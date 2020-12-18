13554b8d046bb0d31b270a99a7c0d10da5a94b5e
phalanx10 <phalanx.1379@gmail.com>
2020-12-18 11:00:31 +0900

    アクセス制限

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,12 +1,13 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :move_to_signin
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index
   def index
-    @item = Item.find(params[:item_id])
     @address_record = AddressRecord.new
   end
 
   def create
     #binding.pry
-    @item = Item.find(params[:item_id])
     @address_record = AddressRecord.new(address_record_params)
     if @address_record.valid?
       @address_record.save
@@ -18,8 +19,19 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
-    def address_record_params
-      params.require(:address_record).permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number,:purchase_record_id, :user_id, :item_id)
-    end
-    
+  def address_record_params
+    params.require(:address_record).permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number,:purchase_record_id, :user_id, :item_id)
+  end
+
+  def move_to_signin
+    authenticate_user! unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user.id
+  end 
 end
